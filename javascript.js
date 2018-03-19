f1c59b86c40db883952f7d97e56fcd1939cc75e2
Your Name <you@example.com>
2018-03-19 15:27:48 +0100

    * Inicio programa * Creación del formulario * No inserta los datos en la tabla

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function AlumnosController($scope) {
+    $scope.alumnos=[
+        {dni:" ", nombre=" ", apellidos=" ", correo=" ", nota=" "}
+    ];
+
+$scope.Save=function(){
+    $scope.alumnos.push({dni:$scope.nuevoAlumno.dni, nombre:$scope.nuevoAlumno.nombre, apellidos:$scope.nuevoAlumno.apellidos, correo:$scope.nuevoAlumno.correo, nota:$scope.nuevoAlumno.nota});
+}
+
+}
